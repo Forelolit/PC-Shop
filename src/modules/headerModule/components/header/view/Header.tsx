42481cb72b041fcd652a54a53f9573ee48054bf7
Your Name <you@example.com>
2025-09-15 12:35:52 +0600

    test commit

--- src/modules/headerModule/components/header/view/Header.tsx
+++ src/modules/headerModule/components/header/view/Header.tsx
@@ -5,14 +5,13 @@ import { Link } from 'react-router';
 import { path } from '@utils/constants/constants';
 
 export const Header: FC = () => {
-    return (
-        <header className={styles.header}>
-             <div className={styles.logo}>
-                
-                <Link to={path.home}>Link</Link>
-            </div>
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={path.home}>Link</Link>
+      </div>
 
-            <Navbar />
-        </header>
-    );
+      <Navbar />
+    </header>
+  );
 };
